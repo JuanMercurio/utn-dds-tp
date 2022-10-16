f72a30a16e421d241d68c690c37dae1410bc8d03
matiascarasi <mcarasi@frba.utn.edu.ar>
2022-10-16 20:08:27 -0300

    elimino node y adapto a html puro

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/solicitudes', function(req, res, next) {
-  res.render('index', { title: 'Solicitudes de Miembros', index: "SolicitudMiembros", cssDependencies: "dependencies/SolicitudMiembros/cssDependencies", jsDependencies: "dependencies/SolicitudMiembros/jsDependencies"});
-});
-
-module.exports = router;
